a906d84cedafc5cc4d408cd4ad6bc4ca9e955bfc
Datruc21 <jalil.bellahcen@gmail.com>
2026-02-13 19:19:52 +0100

    Add files via upload

--- rc.lua
+++ rc.lua
@@ -79,20 +79,11 @@ modkey = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
+    awful.layout.suit.magnifier
 }
 -- }}}
 
@@ -615,7 +606,6 @@ client.connect_signal("manage", function (c)
     -- if not awesome.startup then awful.client.setslave(c) end
     c.border_width = beautiful.border_width
     c.border_color = beautiful.border_normal
-
     if awesome.startup
       and not c.size_hints.user_position
       and not c.size_hints.program_position then
@@ -635,6 +625,7 @@ end)
 -- Bordures focus/unfocus
 client.connect_signal("focus", function(c)
     c.border_color = beautiful.border_focus
+    
     c.border_width = beautiful.border_width 
     end)
 client.connect_signal("unfocus", function(c)
